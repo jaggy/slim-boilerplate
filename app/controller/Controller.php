2c745a90744a7167368db56b8fbba6f8043d5986
Jaggy Gauran <jaggygauran@gmail.com>
2014-05-31 23:54:06 +0800

    update versions

--- app/controller/Controller.php
+++ app/controller/Controller.php
@@ -9,7 +9,7 @@ use Slim\Slim;
  * @package    Controller
  * @author     Jaggy Gauran <jaggygauran@gmail.com>
  * @license    http://www.wtfpl.net/ Do What the Fuck You Want to Public License
- * @version    Release: 0.1.2
+ * @version    Release: 0.1.3
  * @link       http://github.com/jaggyspaghetti/slim-boilerplate
  * @since      Class available since Release 0.1.0
  */
